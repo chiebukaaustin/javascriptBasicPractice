dadb3c91d859d0cb7fd284e63043fe06631c7f45
Obietoh Chiebuka <ebukarox@yahoo.com>
2020-03-16 12:11:35 +0100

    first javascript practice

---
+++ javascriptBegining.js
@@ -0,0 +1,16 @@
+var firstName = prompt("what is your first name")
+var lastName = prompt("what is your last name")
+var Age = Number( prompt("how old are you"))
+var days = Age * 365;
+alert("you are " + days +" days old");
+if (Age <0) {console.log("OO.G. you dey whine me??")}
+if (Age === 21) {console.log("you are now legal, have as much coitus as you want!!")}
+if (Age % 2 !==0) {console.log("you age is odd, you have to be odd to be number 1 :)")}
+if (Age % Math.sqrt ==0) {console.log("your age is a perfect square!!")}
+
+    
+console.log(`Your Fullname is ${firstName} ${lastName}`)
+console.log("you are " + Age + " years old")
+
+
+//console.log("hey iam connected")  
